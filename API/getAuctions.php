cb3b4604009b4f6a04e19178b074b1f715991bf4
Mattw46 <mattw76@live.com.au>
2019-09-17 15:18:57 +1000

    finalised ajax functionality for homepage

--- API/getAuctions.php
+++ API/getAuctions.php
@@ -1,16 +1,27 @@
 <?php
-/** create dummy JSON data to develop and test ajax functionality */
+/** create dummy JSON data to develop and test ajax functionality 
+ * EndsIn is in hours
+*/
 
-$auction1->CurrentBid = "";
-$auction1->EndsIn = "";
+$auction1->id = 1;
+$auction1->Title = "Mow Lawn";
+$auction1->Description = "Mow large yard";
+$auction1->CurrentBid = "50.00";
+$auction1->EndsIn = "2";
 $auction1->TotalBid = 6;
 $auction1->image = "http://placehold.it/200x150";
-$auction2->CurrentBid = "";
-$auction2->EndsIn = "";
+$auction2->id = 2;
+$auction2->Title = "Change light bulb";
+$auction2->Description = "Change 6 light bulbs";
+$auction2->CurrentBid = "20.00";
+$auction2->EndsIn = "5";
 $auction2->TotalBid = 6;
 $auction2->image = "http://placehold.it/200x150";
-$auction3->CurrentBid = "";
-$auction3->EndsIn = "";
+$auction3->id = 3;
+$auction3->Title = "Unclog drain";
+$auction3->Description = "Blocked drain needs unclogging";
+$auction3->CurrentBid = "45.00";
+$auction3->EndsIn = "4";
 $auction3->TotalBid = 6;
 $auction3->image = "http://placehold.it/200x150";
 $auctionList = array($auction1,$auction2,$auction3);
--- js/auctions.js
+++ js/auctions.js
@@ -1,26 +1,6 @@
 function loadAuctions() {
-
-   var auction = '<div class="col-md-4">.col-md-4' +
-                     '<img src="http://placehold.it/200x150">' +
-
-                     '<p class="listing-title"></p>'+
-                                             
-                     '<p class="listing-title"></p>'+
-                                             '<p class="listing-bid">Current Bid: <span class="listing-bid-amount"></span></p>'+
-
-                     '<p class="listing-ends-in">Ends in: <span class="listing-ends-in-amount"></span></p>'+
-
-                     '<p class="listing-total-bids">Total Bids: <span class="listing-total-bids-amount">'+
-                                                      '</span></p><form method="GET" action="./job.php">'+
-                           '<input type="hidden" name="listing_id" value="/">'+
-                           '<button type="submot">Place bid</button>'+
-                        '</form>'+
-                     '<p></p>'+
-                  '</div>'+
-                  '</div>';
-   //document.getElementById("pagebody").innerHTML += "Hello World";
-   //document.getElementById("pagebody").innerHTML += auction;
-   alert("Page is loaded");
+   
+   //alert("Page is loaded");
 
    var xmlhttp = new XMLHttpRequest();
    xmlhttp.onreadystatechange = function() {
@@ -28,11 +8,47 @@ function loadAuctions() {
          var myObj = JSON.parse(this.responseText);
          console.log(myObj);
          console.log(myObj.length);
-         document.getElementById("pagebody").innerHTML += "TotalBid: " + myObj[0].TotalBid + "<br> image: " + myObj[0].image + "<br>";
-         document.getElementById("pagebody").innerHTML += "TotalBid: " + myObj[1].TotalBid + "<br> image: " + myObj[1].image + "<br>";
-         document.getElementById("pagebody").innerHTML += "TotalBid: " + myObj[2].TotalBid + "<br> image: " + myObj[2].image + "<br>";
+
+         for (var i = 0; i < myObj.length; i++) {
+            document.getElementById("pagebody").innerHTML += createAuction(myObj[i]);
+         }
+
+         /*document.getElementById("pagebody").innerHTML += createAuction(myObj[0]);
+         
+         document.getElementById("pagebody").innerHTML += "ID: " + myObj[0].id + " Title: " + myObj[0].Title + " Description: " + 
+            myObj[0].Description + " CurrentBid: " + myObj[0].CurrentBid + " TotalBid: " + myObj[0].TotalBid + "<br> image: " + 
+            myObj[0].image + "<br>";
+         document.getElementById("pagebody").innerHTML += "ID: " + myObj[1].id + " Title: " + myObj[1].Title + " Description: " + 
+            myObj[1].Description + " CurrentBid: " + myObj[1].CurrentBid + " TotalBid: " + myObj[1].TotalBid + "<br> image: " + 
+            myObj[1].image + "<br>";
+         document.getElementById("pagebody").innerHTML += "ID: " + myObj[2].id + " Title: " + myObj[2].Title + " Description: " + 
+            myObj[2].Description + " CurrentBid: " + myObj[2].CurrentBid + " TotalBid: " + myObj[2].TotalBid + "<br> image: " + 
+            myObj[2].image + "<br>";*/
       }
    };
    xmlhttp.open("GET", "API/getAuctions.php", true);
    xmlhttp.send();
+
+;}
+
+/* Takes array element and returns result as HTML element */
+function createAuction(auctionData) {
+   var auction = `<div class="col-md-4">` +
+                  `<img src="${auctionData.image}">` +
+                  `<p class="listing-title">${auctionData.Title}</p>` +                                            
+                  `<p class="listing-description">${auctionData.Description}</p>` +
+                  `<p class="listing-bid">Current Bid: <span class="listing-bid-amount">${auctionData.CurrentBid}</span></p>` +
+                  `<p class="listing-ends-in">Ends in: <span class="listing-ends-in-amount">${auctionData.EndsIn}</span></p>` +
+                  `<p class="listing-total-bids">Total Bids: <span class="listing-total-bids-amount">${auctionData.TotalBid}` +
+                  `</span></p><form method="GET" action="./job.php">` +
+                           `<input type="hidden" name="listing_id" value="${auctionData.id}">` +
+                           `<input type="" name="amount" value=""><br><br>` +
+                           `<button type="submit">Place bid</button>` +
+                           `</form>` +
+                           `<p></p>` +
+                           `</div>` +
+                  `</div>`;
+
+   return auction;
+
 }
