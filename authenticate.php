f1614fe634e69d55a9f689de7ee4b4a395d8587c
Mattw46 <mattw76@live.com.au>
2019-09-19 11:07:39 +1000

    dummy login works and redirects on successful login

--- authenticate.php
+++ authenticate.php
@@ -1,5 +1,12 @@
 <?php
 session_start();
+
+function authenticate($user, $password) {
+  if ($user == "user1" && $password == "password") {
+    return true;
+  }
+  return false;
+}
 ?>
 <!doctype html>
 <html lang="en">
@@ -69,11 +76,21 @@ session_start();
         <div class="jumbotron text-center" style="margin-bottom:0">
         
         <?php
+        $user = $_GET["user"];
+        $password = $_GET["passwd"];
+        if (authenticate($user, $password)) {
           $_SESSION["token"] = "test";
           $_SESSION["authenticated"] = true;
 
           echo "token: " . $_SESSION["token"] . "<br>";
           echo "authenticated" . $_SESSION["authenticated"] . "<br>";
+          header("Location: auctions.php");
+        }
+        else {
+          echo '<h4 style="color: red;">Login Failed try again</h4>';
+          echo "user: ".$user." password: ".$password;
+        }
+          
         ?> 
         <form>
           User Name:<br>
